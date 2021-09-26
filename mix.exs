618e9b0851c53c2afc5301f4d6614c5fc7f19e4c
Curtis Schiewek <curtis.schiewek@hey.com>
2021-09-26 19:54:07 -0400

    add exdocs

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule Foreperson.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
